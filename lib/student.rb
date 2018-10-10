16f1e20727c8f852b2631bfdcd7c00cd5f998e5c
Harry Bloch <harryfbloch@gmail.com>
2018-10-10 18:48:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,9 +19,8 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each {|student|
-      binding.pry 
+      Student.new(student) 
     }
-    Scraper.scrape_profile_page()
   end
 
   def add_student_attributes(attributes_hash)
